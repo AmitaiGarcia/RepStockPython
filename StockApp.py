6af8dc19fc747e80ff6a5b14b0f80d88a501dc7a
Amitai <Amitaigarcia75@gmail.com>
2023-01-04 12:29:46 +0200

    added some extra Gui to help the user

--- StockApp.py
+++ StockApp.py
@@ -3,12 +3,14 @@ import functions
 
 # GUI Definition
 layout = [
+    [sg.Text("Welcome to the stock finder App", font=("Arial", 20))],
     [sg.Text("Please enter the stock symbol:"), sg.Input(key="stock_name")],
     [sg.Text("Please enter the start date in DD/MM/YYYY format:"),
      sg.Input(key="start_date")],
     [sg.Text("Please enter the end date in DD/MM/YYYY format:"),
      sg.Input(key="end_date")],
-    [sg.Exit(), sg.Button("Find"), sg.Button("Find 3 top performing stocks")],
+    [sg.Exit(button_color="tomato"), sg.Button("Find stock information"),
+     sg.Button("Find 3 top performing stocks"), sg.Button("How it works")],
 
 ]
 
@@ -19,7 +21,7 @@ while True:
     print(event, values)
     if event in (sg.WINDOW_CLOSED, "Exit"):
         break
-    if event == "Find":
+    if event == "Find stock information":
         if (functions.is_valid_date(values["start_date"]) and functions.is_valid_date(values["end_date"])):
             functions.display_max_min(
                 values["stock_name"], values["start_date"], values["end_date"])
@@ -27,3 +29,5 @@ while True:
         if (functions.is_valid_date(values["start_date"]) and functions.is_valid_date(values["end_date"])):
             functions.calc_top3(
                 values["start_date"], values["end_date"])
+    if event == "How it works":
+        sg.Popup("Hi!\n\nTo find information about a specific stock.\nFill in all the information and click the 'Find stock information' button\n\nTo find the 3 best performing stocks within a time period.\nFill in the dates and click the 'Find 3 top perfoming stocks' button.\n\nTo Exit... click the exit button!", title="Help!")
